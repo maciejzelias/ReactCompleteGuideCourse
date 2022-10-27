5ceeb54e156a665899990bebbd353cd324185551
Maciej <s22482@pjwstk.edu.pl>
2022-10-27 21:24:24 +0200

    update12

--- loginProject/src/components/MainHeader/Navigation.js
+++ loginProject/src/components/MainHeader/Navigation.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import classes from './Navigation.module.css';
+import AuthContext from '../context/auth-context';
 
-const Navigation = (props) => {
-  return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
+const Navigation = () => {
+
+  const context = useContext(AuthContext);
+
+  // return (
+  //   <AuthContext.Consumer>
+  //     {(context) => {
+  return (<nav className={classes.nav}>
+    <ul>
+      {context.isLoggedIn && (
+        <li>
+          <a href="/">Users</a>
+        </li>
+      )}
+      {context.isLoggedIn && (
+        <li>
+          <a href="/">Admin</a>
+        </li>
+      )}
+      {context.isLoggedIn && (
+        <li>
+          <button onClick={context.onLogout}>Logout</button>
+        </li>
+      )}
+    </ul>
+  </nav>
+    // </AuthContext.Consumer>
   );
 };
 
