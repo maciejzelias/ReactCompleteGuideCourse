53c7ce508c67f970bfd51ca80157442ea58ab973
Maciej <s22482@pjwstk.edu.pl>
2022-11-18 18:38:50 +0100

    sendind data to firebase

---
+++ advanced-redux/src/App.js
@@ -0,0 +1,80 @@
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, Fragment } from "react";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
+
+function App() {
+  const dispatch = useDispatch();
+
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+
+  const notification = useSelector((state) => state.ui.notification);
+
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "sending...",
+          message: "sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "https://react-http-f1f33-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending Cart data failed");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully",
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed !",
+        })
+      );
+    });
+  }, [cart, dispatch]);
+
+  return (
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
+  );
+}
+
+export default App;
